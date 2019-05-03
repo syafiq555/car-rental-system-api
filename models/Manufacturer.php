8be13a1526e3c21aeaad904fee964fd0c4d51d2f
syafiq shamsuddin <syafiq555@rocketmail.com>
2019-05-03 23:59:28 +0800

    tertinggal

--- models/Manufacturer.php
+++ models/Manufacturer.php
@@ -8,12 +8,4 @@ class Manufacturer extends Base{
   public function __construct() {
     
   }
-
-  public function __get($name) {
-    return $this->$name;
-  }
-
-  public function __set($name, $value) {
-      $this->$name = $value;
-  }
 }
