876acfd3a76e5b7a8a10cad95249dd946a73b66f
syafiq shamsuddin <Shyuffix555@gmail.com>
2019-05-05 18:28:17 +0800

    modify models to include in json

--- models/Car.php
+++ models/Car.php
@@ -5,10 +5,14 @@ class Car implements iGetManufacturer, iGetModel {
   public $plate_number;
   public $price_per_hour;
   private $model;
+  public $model_name;
+  public $manufacturer_name;
   public $year;
 
   public function __construct(Model $model) {
     $this->model = $model;
+    $this->manufacturer_name = $model->getManufacturer();
+    $this->model_name = $model->model_name;
   }
 
   public function getModel() {
--- models/Model.php
+++ models/Model.php
@@ -4,10 +4,12 @@ include_once('Manufacturer.php');
 class Model implements iGetManufacturer{
   public $id;
   public $model_name;
+  public $manufacturer_name;
   private $manufacturer;
 
   public function __construct(Manufacturer $manufacturer) {
     $this->manufacturer = $manufacturer;
+    $this->manufacturer_name = $manufacturer->manufacturer_name;
   }
 
   public function getManufacturer() {
